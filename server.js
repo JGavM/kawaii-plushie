8e2e9e5b240a223e9e6a6bf4fc8b8c5e6f1ab034
JGavM <42217000+JGavM@users.noreply.github.com>
2020-03-29 20:25:47 -0600

    Updates on Customer API (Test)

--- server.js
+++ server.js
@@ -12,10 +12,92 @@ const dataBaseConfig ={
 app.use(express.static(__dirname + "/public/dist/cutie-plushie"));
 
 /**
- * Data View API
+ * -----------------------------------------
+ * Customer API v1
+ * -----------------------------------------
  */
 
-app.get('/api/v1/web/dataview/getavatars', function (req, res) {
+/**
+ *  GET Methods
+ */
+
+ //Get avatars
+app.get('/api/v1/web/avatars', function (req, res) {
+  mssql.connect(dataBaseConfig, function (err) {
+    
+    if (err){
+      console.log(err);
+    }
+
+    let request = new mssql.Request();
+       
+    // Query to the database and get the records
+    request.query('SELECT * FROM Avatars', function (err, records) {
+        
+        if (err){
+          console.log(err);
+        }
+
+        // Send records as a response
+        res.send(records.recordset);
+        
+    });
+  });
+});
+
+app.get('/api/v1/web/avatars/:avatarId', function (req, res) {
+  mssql.connect(dataBaseConfig, function (err) {
+    
+    if (err){
+      console.log(err);
+    }
+
+    let request = new mssql.Request();
+    let avatarId = req.params.avatarId;
+       
+    // Query to the database and get the records
+    request.query('SELECT * FROM Avatars WHERE "Avatar_ID" = ' + avatarId, function (err, records) {
+        
+        if (err){
+          console.log(err);
+        }
+
+        // Send records as a response
+        res.send(records.recordset);
+        
+    });
+  });
+});
+
+//Get currency rates
+
+/**
+ *  POST Methods
+ */
+
+  
+/**
+ * -----------------------------------------
+ * Manager API v1
+ * -----------------------------------------
+ */
+
+ /**
+ * User Management API
+ */
+
+app.post('/api/v1/management/usermanagement/',function(req,res){
+
+});
+
+/**
+ * -----------------------------------------
+ * Supplier API v1
+ * -----------------------------------------
+ */
+
+  //Get Avatars
+app.get('/api/v1/supplier/getavatars', function (req, res) {
   mssql.connect(dataBaseConfig, function (err) {
     
     if (err){
@@ -39,13 +121,40 @@ app.get('/api/v1/web/dataview/getavatars', function (req, res) {
 });
 
 /**
- * User Management API
+ * -----------------------------------------
+ * Distributor API v1
+ * -----------------------------------------
  */
 
-app.post('/api/v1/web/usermanagement/',function(req,res){
+  //Get Avatars
+app.get('/api/v1/distributor/getavatars', function (req, res) {
+  mssql.connect(dataBaseConfig, function (err) {
+    
+    if (err){
+      console.log(err);
+    }
 
+    let request = new mssql.Request();
+       
+    // query to the database and get the records
+    request.query('SELECT * FROM Avatars', function (err, records) {
+        
+        if (err){
+          console.log(err);
+        }
+
+        // send records as a response
+        res.send(records);
+        
+    });
+  });
 });
-  
+
+/**
+ * -----------------------------------------
+ * Server start on port 8080
+ * -----------------------------------------
+ */
 app.listen(8080, function () {
     console.log("Escuchando el puerto 8080...");
 });
