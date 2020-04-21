91d670d35bc592900430213f575273ca711d3725
JGavM <42217000+JGavM@users.noreply.github.com>
2020-04-21 00:00:16 -0500

    Sales completed

--- public/src/app/management/management-sales/management-sales.component.ts
+++ public/src/app/management/management-sales/management-sales.component.ts
@@ -55,10 +55,11 @@ export class ManagementSalesComponent implements OnInit {
     'articleId',
     'productId',
     'productName',
+    'productUnitPriceMXN',
+    'saleAppliedDiscount',
     'orderId',
     'orderStatus',
     'customerName',
-    'deliveryId',
     'deliveryDate',
     'expectedArrivalDate',
     'actualArrivalDate',
@@ -90,13 +91,35 @@ export class ManagementSalesComponent implements OnInit {
   applyFilter(event: Event) {
     // Apply a filter to the dataSource after user input
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filterPredicate = (data, filter) => JSON.stringify(data).includes(filter);
+    this.dataSource.filterPredicate = (data, filter: string)  => {
+      const accumulator = (currentTerm, key) => {
+        return this.nestedFilterCheck(currentTerm, data, key);
+      };
+      const dataStr = Object.keys(data).reduce(accumulator, '').toLowerCase();
+      // Transform the filter by converting it to lowercase and removing whitespace.
+      const transformedFilter = filter.trim().toLowerCase();
+      return dataStr.indexOf(transformedFilter) !== -1;
+    };
+
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
     // Return to first page after applying a filter
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
+  }
+
+  nestedFilterCheck(search, data, key) {
+    if (typeof data[key] === 'object') {
+      for (const k in data[key]) {
+        if (data[key][k] !== null) {
+          search = this.nestedFilterCheck(search, data[key], k);
+        }
+      }
+    } else {
+      search += data[key] + ' ';
+    }
+    return search;
   }
 
   // Related to back-end sale management
@@ -125,7 +148,7 @@ export class ManagementSalesComponent implements OnInit {
           .set("Authorization", "Bearer " + token);
 
     return this.http.get(
-      '/api/v1/management/sales',
+      '/api/v1/management/sales/?page=1&salesPerPage=1000000',
       {headers: headers, observe: 'response', responseType: 'json'}
     ).toPromise().then(function(res) {
       return res;
